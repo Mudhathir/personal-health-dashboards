e84e3fd2bd9e005438492b8feb073f9c3b993154
Mudhathir Sharif Khaja <67299805+Mudhathir@users.noreply.github.com>
2025-04-11 23:38:14 -0400

    Create PersonalHealthDashboardsApp.swift

---
+++ PersonalHealthDashboardsApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI  // Import SwiftUI framework
+
+@main
+struct PersonalHealthDashboardsApp: App {  // Main entry point for the app
+    var body: some Scene {
+        WindowGroup {
+            HealthDashboardView()  // Set the main content view to HealthDashboardView
+        }
+    }
+}
